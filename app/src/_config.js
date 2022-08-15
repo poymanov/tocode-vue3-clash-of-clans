578c848f507e34cb6cd2553476b9931beb16d5c8
poymanov <n.poymanov@gmail.com>
2022-08-15 18:41:21 +0300

    Добавлена информационная страница "О приложении"

---
+++ app/src/_config.js
@@ -0,0 +1,16 @@
+export const app = {
+  title: "Clash of Clans",
+  courseUrl: "https://tocode.ru/courses/vuejs-3-s-nulya-do-rezultata/",
+  menu: [
+    {
+      alias: "home",
+      url: "/",
+      title: "Home",
+    },
+    {
+      alias: "about",
+      url: "/about",
+      title: "About",
+    },
+  ],
+};
--- app/src/components/layout/HeaderContent.vue
+++ app/src/components/layout/HeaderContent.vue
@@ -3,10 +3,12 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <RouterLink to="/" class="navbar-logo">Clash of Clans</RouterLink>
+          <RouterLink to="/" class="navbar-logo">{{ title }}</RouterLink>
           <ul class="navbar-list">
-            <li class="navbar-item">
-              <RouterLink to="/" class="navbar-link">Home</RouterLink>
+            <li class="navbar-item" v-for="item in menu" :key="item.alias">
+              <RouterLink :to="item.url" class="navbar-link">{{
+                item.title
+              }}</RouterLink>
             </li>
           </ul>
         </div>
@@ -14,3 +16,16 @@
     </div>
   </header>
 </template>
+
+<script>
+import { app } from "@/_config.js";
+
+export default {
+  data() {
+    return {
+      title: app.title,
+      menu: app.menu,
+    };
+  },
+};
+</script>
